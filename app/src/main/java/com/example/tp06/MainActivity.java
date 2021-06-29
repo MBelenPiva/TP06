8d95cfb1075bc48278b4dccbeace60adc4157fba
AngiPark <83955402+AngiPark@users.noreply.github.com>
2021-06-29 19:56:20 -0300

    26/06

--- app/src/main/java/com/example/tp06/MainActivity.java
+++ app/src/main/java/com/example/tp06/MainActivity.java
@@ -3,13 +3,54 @@ package com.example.tp06;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    private
+    private EditText edtNombre;
+    private Button btnContinuar;
+
+    MapaFragment mapaFragment;
+    AcercaDeFragment acercaDeFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ObtenerReferencias();
+
+        SetearListeners();
+
+        createFragments();
+
+        showInitialFragment();
+    }
+
+    private void ObtenerReferencias(){
+        edtNombre = findViewById(R.id.edtNombre);
+        btnContinuar = findViewById(R.id.btnContinuar);
+    }
+
+    private void SetearListeners(){
+        btnContinuar = findViewById(btnContinuar_Click);
+    }
+
+    private View.OnClickListener btnContinuar_Click = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            goToFragmentWithReplace(acercaDeFragment, true);
+        }
+    };
+
+    private void createFragments(){
+        mapaFragment = new MapaFragment();
+        acercaDeFragment = new AcercaDeFragment();
+    }
+
+    private void showInitialFragment(){
+        goToFragmentWithReplace(mapaFragment, true);
     }
 }
