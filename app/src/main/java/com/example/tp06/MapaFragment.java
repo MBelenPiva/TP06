bbff42445d67f01e0c95b3f98c71e7ea9fbcb80c
MBelenPiva <83927735+MBelenPiva@users.noreply.github.com>
2021-06-30 08:05:19 -0300

    Utilis y Bases

--- app/src/main/java/com/example/tp06/MapaFragment.java
+++ app/src/main/java/com/example/tp06/MapaFragment.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.example.tp06.Utils.LogHelper;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 
---
+++ app/src/main/java/com/example/tp06/Utils/LogHelper.java
@@ -0,0 +1,16 @@
+package com.example.tp06.Utils;
+
+import android.util.Log;
+
+public class LogHelper {
+    public static final String LOG_TAG = "";
+
+    public static void d(String strMensaje){
+        Log.d(LOG_TAG, strMensaje);
+    }
+
+    public static void enArchivo(String message){
+
+    }
+
+}
